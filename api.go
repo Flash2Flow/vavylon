5fe98b98ce5c2f5c0e9c8165b8d82eced61f327c
re-incarnation <re-incarnation@bk.ru>
2021-10-10 03:46:42 +1000

    test Fprintf with massive

--- api.go
+++ api.go
@@ -1,12 +1,63 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
-func api(w http.ResponseWriter, r *http.Request)  {
+func api(w http.ResponseWriter, r *http.Request) {
+	err_url := [...]string{
+		"Error 101:\nBad title ( Empty )",
+		"Error 102:\nBad token",
+	}
+
 	log.Print("new request")
-	//querry := r.URL.Query()
-	r.Header.Set("User-Agent", "11")
+	query := r.URL.Query()
+
+	title_byte := query.Get("title")
+	token_byte := query.Get("token")
+
+	token := string(token_byte[:])
+	title := string(title_byte[:])
+
+	if title == "" {
+		log.Println(err_url[0])
+		fmt.Fprintf(w, err_url[0])
+	}
+
+	if token == "" {
+		log.Println(err_url[1])
+		fmt.Fprintf(w, err_url[1])
+	}
+
+	if title == "drop_pass" {
+		if token == "cardinal" {
+
+		}
+
+		if token == "quinella" {
+
+		}
+	}
+
+	if title == "change_pers" {
+		if token == "" {
+
+		}
+
+		if token == "" {
+
+		}
+	}
+
+	if title == "" {
+		if token == "" {
+
+		}
+
+		if token == "" {
+
+		}
+	}
 }
