f0ea0e52981447dc186de61bc3f0d98f48fd3c64
re-incarnation <74276034+re-incarnation@users.noreply.github.com>
2021-10-10 00:45:37 +1000

    Update server.go

--- server.go
+++ server.go
@@ -3,12 +3,12 @@ package main
 import (
 	"log"
 	"net/http"
-	//"os"
+	"os"
 )
 
 func server() {
-	//port := os.Getenv("PORT")
-	log.Println("server start with port: "/*+port*/)
+	port := os.Getenv("PORT")
+	log.Println("server start with port: "+port)
 	http.HandleFunc("/api", api)
-	http.ListenAndServe(":8080"/*+port*/, nil)
+	http.ListenAndServe(":"+port, nil)
 }
